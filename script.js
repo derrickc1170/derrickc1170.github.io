2d946380ce05ab1fd08484cc0b0b7bb8345aecb4
Derrick Chen <derrickc54@nycstudents.net>
2024-09-27 14:09:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Derrick Chen"; // update the part BETWEEN the "quotes"
+let userName = "derrickc1170"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
